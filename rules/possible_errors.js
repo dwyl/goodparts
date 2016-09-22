1005af6793c3208ed047d367e34a4de039147466
Jack Rans <jwrans@gmail.com>
2016-09-22 14:39:17 +0100

    :heavy_plus_sign: no-ex-assign #29

--- rules/possible_errors.js
+++ rules/possible_errors.js
@@ -2,5 +2,6 @@ module.exports = {
   'no-console': 'off', // https://github.com/dwyl/goodparts/issues/16
   'no-debugger': 'error', // https://github.com/dwyl/goodparts/issues/23
   'no-dupe-keys': ['error', 'never'], // https://github.com/dwyl/goodparts/issues/24
-  'no-empty-character-class': ['error', 'never'] // 'https://github.com/dwyl/goodparts/issues/26'
+  'no-empty-character-class': ['error', 'never'], // 'https://github.com/dwyl/goodparts/issues/26'
+  'no-ex-assign': ['error', 'never'] // 'https://github.com/dwyl/goodparts/issues/29'
 };
--- test/fixtures/possible_errors.js
+++ test/fixtures/possible_errors.js
@@ -4,5 +4,6 @@ module.exports = {
   'no-console': null,
   'no-debugger': { fail: ['debugger;'] },
   'no-dupe-keys': { fail: ['var x = {a:1, a: "repeat"'] },
-  'no-empty-character-class': { fail: ['/^abc[]/.test("abcdefg");'] }
+  'no-empty-character-class': { fail: ['/^abc[]/.test("abcdefg");'] },
+  'no-ex-assign': { fail: ['try { var x; } catch (e) { e = 10; }'] }
 };
