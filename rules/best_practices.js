b6a110d550f0dff7d47668e1a5b114652329ff58
Jack Rans <jwrans@gmail.com>
2016-09-22 17:55:44 +0100

    :heavy_plus_sign: consistent-return #60

--- rules/best_practices.js
+++ rules/best_practices.js
@@ -1,4 +1,5 @@
 module.exports = {
   'array-callback-return': 'error', // https://github.com/dwyl/goodparts/issues/57
   'class-methods-use-this': 'off',// https://github.com/dwyl/goodparts/issues/58
+  'consistent-return': 'error', // https://github.com/dwyl/goodparts/issues/60
 };
--- test/fixtures/best_practices.js
+++ test/fixtures/best_practices.js
@@ -2,5 +2,6 @@ var read = require('../read.js');
 
 module.exports = {
   'array-callback-return': { fail: ['[1].map(function(x){ console.log(x)})'] },
-  'class-methods-use-this': null, // https://github.com/dwyl/goodparts/issues/58
+  'class-methods-use-this': null,
+  'consistent-return': { fail: ['function f(c) { if (c) { return true; } }'] },
 };
