e0619a91a9f38bb43734d81da8a252bfb09addfb
Jack Rans <jwrans@gmail.com>
2016-09-22 14:33:20 +0100

    :heavy_plus_sign: no-empty-character-class #26

--- rules/possible_errors.js
+++ rules/possible_errors.js
@@ -1,5 +1,6 @@
 module.exports = {
   'no-console': 'off', // https://github.com/dwyl/goodparts/issues/16
   'no-debugger': 'error', // https://github.com/dwyl/goodparts/issues/23
-  'no-dupe-keys': ['error', 'never'] // https://github.com/dwyl/goodparts/issues/24
+  'no-dupe-keys': ['error', 'never'], // https://github.com/dwyl/goodparts/issues/24
+  'no-empty-character-class': ['error', 'never'] // 'https://github.com/dwyl/goodparts/issues/26'
 };
--- test/fixtures/possible_errors.js
+++ test/fixtures/possible_errors.js
@@ -2,6 +2,7 @@ var read = require('../read.js');
 
 module.exports = {
   'no-console': null,
-  'no-debugger': { pass: [], fail: ['debugger;'] },
-  'no-dupe-keys': { pass: [], fail: ['var x = {a:1, a: "repeat"']}
+  'no-debugger': { fail: ['debugger;'] },
+  'no-dupe-keys': { fail: ['var x = {a:1, a: "repeat"'] },
+  'no-empty-character-class': { fail: ['/^abc[]/.test("abcdefg");'] }
 };
