e8a3564223941bb715755eb7527d0861cac78d1a
Jack Rans <jwrans@gmail.com>
2016-09-22 13:16:55 +0100

    :heavy_plus_sign: no-dupe-keys #24

--- rules/possible_errors.js
+++ rules/possible_errors.js
@@ -1,4 +1,5 @@
 module.exports = {
-  "no-console": "off", // https://github.com/dwyl/goodparts/issues/16
-  "no-debugger": "error" // https://github.com/dwyl/goodparts/issues/23
+  'no-console': 'off', // https://github.com/dwyl/goodparts/issues/16
+  'no-debugger': 'error', // https://github.com/dwyl/goodparts/issues/23
+  'no-dupe-keys': ['error', 'never'] // https://github.com/dwyl/goodparts/issues/24
 };
