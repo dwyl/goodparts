d851ab78044f407371430c708f12466cca6efd23
Elias <elias0789@gmail.com>
2016-09-22 19:18:41 +0100

    setup test fixtures for possible error rules (no-irregular-whitespace currently failing; investigating)

--- test/fixtures/possible_errors.js
+++ test/fixtures/possible_errors.js
@@ -1,3 +1,71 @@
 var read = require('../read.js');
 
-module.exports = {};
+module.exports = {
+  'valid-jsdoc': {
+    fail: [
+      read('./valid-jsdoc.fail')
+    ],
+    pass: [
+      read('./valid-jsdoc.pass')
+    ]
+  },
+
+  'no-irregular-whitespace': {
+    fail: [
+      read('./no-irregular-whitespace.fail')
+    ]
+  },
+
+  'no-func-assign': {
+    fail: [
+      'function foo () {}; foo = 1;'
+    ]
+  },
+
+  "no-extra-boolean-cast": {
+    fail: [
+      '!!!foo',
+      'Boolean(!!foo)',
+      'if (!!foo) {console.log(foo);}'
+    ]
+  },
+
+  "no-empty": {
+    fail: [
+      'if (true) {}',
+      'while (1) {}',
+      'for (var i = 0; i < 2; i++) {}'
+    ]
+  },
+
+  "no-duplicate-case": {
+    fail: [
+      read('./no-duplicate-case.fail')
+    ]
+  },
+
+  "no-dupe-args": {
+    fail: [
+      'function (a, a) {};'
+    ]
+  },
+
+  "no-control-regex": {
+    fail: [
+      'var x = /\x1f/',
+      'var x = new RegExp("/\x1f/")'
+    ]
+  },
+
+  "no-constant-condition": {
+    fail: [
+      'while (1) {}'
+    ]
+  },
+
+  "no-cond-assign": {
+    fail: [
+      'if (foo = 1) { foo++; }'
+    ]
+  }
+};
