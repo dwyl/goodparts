75729d0fab701a0eb8d764e84383df53e929b2f0
Jack Rans <jwrans@gmail.com>
2016-09-22 14:42:30 +0100

    :heavy_plus_sign: no-extra-parens #31

--- rules/possible_errors.js
+++ rules/possible_errors.js
@@ -3,5 +3,6 @@ module.exports = {
   'no-debugger': 'error', // https://github.com/dwyl/goodparts/issues/23
   'no-dupe-keys': ['error', 'never'], // https://github.com/dwyl/goodparts/issues/24
   'no-empty-character-class': ['error', 'never'], // 'https://github.com/dwyl/goodparts/issues/26'
-  'no-ex-assign': ['error', 'never'] // 'https://github.com/dwyl/goodparts/issues/29'
+  'no-ex-assign': ['error', 'never'], // 'https://github.com/dwyl/goodparts/issues/29'
+  'no-extra-parens': 'off', // https://github.com/dwyl/goodparts/issues/31
 };
--- test/fixtures/possible_errors.js
+++ test/fixtures/possible_errors.js
@@ -5,5 +5,6 @@ module.exports = {
   'no-debugger': { fail: ['debugger;'] },
   'no-dupe-keys': { fail: ['var x = {a:1, a: "repeat"'] },
   'no-empty-character-class': { fail: ['/^abc[]/.test("abcdefg");'] },
-  'no-ex-assign': { fail: ['try { var x; } catch (e) { e = 10; }'] }
+  'no-ex-assign': { fail: ['try { var x; } catch (e) { e = 10; }'] },
+  'no-extra-parens': null,
 };
