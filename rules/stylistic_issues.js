88a742727b32caefdf1b2cad817e852490e21bb4
SimonLab <simon.labondance@gmail.com>
2016-09-22 15:31:01 +0100

    add some rules from elias issues

--- rules/stylistic_issues.js
+++ rules/stylistic_issues.js
@@ -3,4 +3,12 @@ module.exports = {
   "max-len": ["error", 80], // https://github.com/dwyl/goodparts/issues/186
   "array-bracket-spacing": ["error", "never"], // https://github.com/dwyl/goodparts/issues/219
   "block-spacing": "error", // https://github.com/dwyl/goodparts/issues/218
+  "brace-style": ["error", "1tbs", { "allowSingleLine": true }], // https://github.com/dwyl/goodparts/issues/217
+  "camelcase": "error", // https://github.com/dwyl/goodparts/issues/217
+  "comma-style": ["error", "last"], // https://github.com/dwyl/goodparts/issues/216
+  "comma-spacing": ["error", { "before": false, "after": true }], // https://github.com/dwyl/goodparts/issues/215
+  "comma-dangle": ["error", "never"], // https://github.com/dwyl/goodparts/issues/213
+  "computed-property-spacing": ["error", "never"], // https://github.com/dwyl/goodparts/issues/212
+  "consistent-this": "off", // https://github.com/dwyl/goodparts/issues/211
+  "eol-last": "error", // https://github.com/dwyl/goodparts/issues/210
 };
--- test/fixtures/stylistic_issues.js
+++ test/fixtures/stylistic_issues.js
@@ -16,5 +16,34 @@ module.exports = {
   'block-spacing': {
     fail: ['{var a =2; }'],
     pass: ['{var a =2;}']
-  }
+  },
+  'brace-style': {
+    fail: [read('brace-style.fail')],
+    pass: [read('brace-style.pass')]
+  },
+  'camelcase': {
+    fail: ['var camel_case = 42;'],
+    pass: ['var camelCase = 42;']
+  },
+  'comma-style': {
+    fail: [read('comma-style.fail')],
+    pass: [read('comma-style.pass')]
+  },
+  'comma-spacing': {
+    fail: ['var a = 2 , b = 3;'],
+    pass: ['var a = 2, b = 3;']
+  },
+  'comma-dangle': {
+    fail: ['var a = [1, 2,];'],
+    pass: ['var a = [1, 2];']
+  },
+  'computed-property-spacing': {
+    fail: ['obj[ "foo" ];'],
+    pass: ['obj["foo"];']
+  },
+  'consistent-this': null,
+  'eol-last': {
+    fail: [read('eol.fail')],
+    pass: [read('eol.pass')]
+  },
 };
