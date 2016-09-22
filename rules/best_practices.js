7d6a7107a52711426ef128336b4d7460341c63a6
Jack Rans <jwrans@gmail.com>
2016-09-22 17:59:47 +0100

    :heavy_plus_sign: default-case #62

--- rules/best_practices.js
+++ rules/best_practices.js
@@ -2,4 +2,5 @@ module.exports = {
   'array-callback-return': 'error', // https://github.com/dwyl/goodparts/issues/57
   'class-methods-use-this': 'off',// https://github.com/dwyl/goodparts/issues/58
   'consistent-return': 'error', // https://github.com/dwyl/goodparts/issues/60
+  'default-case': 'error', // https://github.com/dwyl/goodparts/issues/62
 };
--- test/fixtures/best_practices.js
+++ test/fixtures/best_practices.js
@@ -4,4 +4,5 @@ module.exports = {
   'array-callback-return': { fail: ['[1].map(function(x){ console.log(x)})'] },
   'class-methods-use-this': null,
   'consistent-return': { fail: ['function f(c) { if (c) { return true; } }'] },
+  'default-case': { fail: ['switch (a) { case 1: break; }'] }
 };
