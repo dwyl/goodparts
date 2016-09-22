540cce3253e2ce0cbd7d30e61051691f0b3fc1a4
Jack Rans <jwrans@gmail.com>
2016-09-22 15:55:47 +0100

    :bug: Fix invalid config value #43 #33 #29 #24 #26

--- rules/possible_errors.js
+++ rules/possible_errors.js
@@ -1,14 +1,14 @@
 module.exports = {
   'no-console': 'off', // https://github.com/dwyl/goodparts/issues/16
   'no-debugger': 'error', // https://github.com/dwyl/goodparts/issues/23
-  'no-dupe-keys': ['error', 'never'], // https://github.com/dwyl/goodparts/issues/24
-  'no-empty-character-class': ['error', 'never'], // 'https://github.com/dwyl/goodparts/issues/26'
-  'no-ex-assign': ['error', 'never'], // 'https://github.com/dwyl/goodparts/issues/29'
+  'no-dupe-keys': 'error', // https://github.com/dwyl/goodparts/issues/24
+  'no-empty-character-class': 'error', // 'https://github.com/dwyl/goodparts/issues/26'
+  'no-ex-assign': 'error', // 'https://github.com/dwyl/goodparts/issues/29'
   'no-extra-parens': 'off', // https://github.com/dwyl/goodparts/issues/31
-  'no-extra-semi': ['error', 'never'], // https://github.com/dwyl/goodparts/issues/33
+  'no-extra-semi': 'error', // https://github.com/dwyl/goodparts/issues/33
   'no-inner-declarations': ['error', 'both'], // https://github.com/dwyl/goodparts/issues/40
   'no-invalid-regexp': 'error', // https://github.com/dwyl/goodparts/issues/41
   'no-obj-calls': 'error', // https://github.com/dwyl/goodparts/issues/42
-  'no-prototype-builtins': 'off', // https://github.com/dwyl/goodparts/issues/43
+  'no-prototype-builtins': 'off', // https://github.com/dwyl/goodparts/issues/43,
   'no-sparse-arrays': 'error' //https://github.com/dwyl/goodparts/issues/44
 };
