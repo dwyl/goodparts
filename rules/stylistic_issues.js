fa40824acea4131880a001f517dd33a7e5e35c73
SimonLab <simon.labondance@gmail.com>
2016-09-22 14:46:03 +0100

    test elias rules

--- rules/stylistic_issues.js
+++ rules/stylistic_issues.js
@@ -1,4 +1,6 @@
 module.exports = {
   "space-infix-ops": ["error", {"int32Hint": false}], // https://github.com/dwyl/goodparts/issues/32
   "max-len": ["error", 80], // https://github.com/dwyl/goodparts/issues/186
+  "array-bracket-spacing": ["error", "never"], // https://github.com/dwyl/goodparts/issues/219
+  "block-spacing": "error", // https://github.com/dwyl/goodparts/issues/218
 };
--- test/fixtures/stylistic_issues.js
+++ test/fixtures/stylistic_issues.js
@@ -8,5 +8,13 @@ module.exports = {
   'space-infix-ops': {
     fail: ['var a = 1+ 2;'],
     pass: ['var a = 1 + 2;']
+  },
+  'array-bracket-spacing': {
+    fail: ['[ 1, 2, 3 ]'],
+    pass: ['[1, 2, 3]']
+  },
+  'block-spacing': {
+    fail: ['{var a =2; }'],
+    pass: ['{var a =2;}']
   }
 };
