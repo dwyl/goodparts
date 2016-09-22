a980d5d1c70ffbf445786bd7013ee4586bc1df3c
SimonLab <simon.labondance@gmail.com>
2016-09-22 16:55:58 +0100

    more rules!

--- rules/node_common_js.js
+++ rules/node_common_js.js
@@ -5,4 +5,9 @@ module.exports = {
   "no-process-exit": "error", // https://github.com/dwyl/goodparts/issues/136
   "no-process-env": "off", // https://github.com/dwyl/goodparts/issues/134
   "no-path-concat": "error", // https://github.com/dwyl/goodparts/issues/132
+  "no-new-require": "error", // https://github.com/dwyl/goodparts/issues/129
+  "no-mixed-requires": ["error", { "grouping": true, "allowCall": false }], // https://github.com/dwyl/goodparts/issues/126
+  "handle-callback-err": ["error", "^(error|err\d*)$"], // https://github.com/dwyl/goodparts/issues/121
+  "global-require": "error", // https://github.com/dwyl/goodparts/issues/119
+  "callback-return": ["error", ["done", "next", "callback", "cb"]], // https://github.com/dwyl/goodparts/issues/112
 };
--- test/fixtures/node_common_js.js
+++ test/fixtures/node_common_js.js
@@ -10,5 +10,22 @@ module.exports = {
   'no-process-env': null,
   'no-path-concat': {
     fail: ['var fullPath = __dirname + "/foo.js";']
+  },
+  'no-new-require': {
+    fail: ['var mod = new require("mod");']
+  },
+  'no-mixed-requires': {
+    fail: [read('no-mixed-requires.fail')],
+  },
+  'handle-callback-err': {
+    fail: [read('handle-callback-err.fail')],
+    pass: [read('handle-callback-err.pass')]
+  },
+  'global-require': {
+    fail: [read('global-require.fail')]
+  },
+  'callback-return': {
+    fail: [read('callback-return.fail')],
+    pass: [read('callback-return.pass')]
   }
 };
