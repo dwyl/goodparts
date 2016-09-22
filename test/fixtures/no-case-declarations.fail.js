413cf2af4c216266cb375cf8fa48f6ee088599ad
Elias <elias0789@gmail.com>
2016-09-22 18:32:01 +0100

    specify test fixtures for best-practise rules

---
+++ test/fixtures/no-case-declarations.fail.js
@@ -0,0 +1,13 @@
+switch (foo) {
+  case 1:
+    let x = 1;
+    break;
+  case 2:
+    const y = 2;
+    break;
+  case 3:
+    function f() {}
+    break;
+  default:
+    class C {}
+}
---
+++ test/fixtures/no-case-declarations.pass.js
@@ -0,0 +1,9 @@
+switch (foo) {
+  case 1: {
+    foo + 1;
+    break;
+  }
+  default: {
+    foo + 0;
+  }
+}
