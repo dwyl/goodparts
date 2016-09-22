c9676dac7b61cc9710ac61430da14b859a8b4fa9
Jack Rans <jwrans@gmail.com>
2016-09-22 16:10:39 +0100

    :heavy_plus_sign: max-len (redone) #186

--- test/fixtures/stylistic_issues.js
+++ test/fixtures/stylistic_issues.js
@@ -2,8 +2,8 @@ var read = require('../read.js');
 
 module.exports = {
   'max-len': {
-    fail: [read('max-len.fail')],
-    pass: ['aaaaaa']
+    fail: [read('max-len.fail0'), read('max-len.fail1'), read('max-len.fail2')],
+    pass: [read('max-len.pass0'), read('max-len.pass1')]
   },
   'space-infix-ops': {
     fail: ['var a = 1+ 2;'],
