30b554ebafaf8ab1d94143d31f28ed3b5529ac1d
Jack Rans <jwrans@gmail.com>
2016-09-22 18:06:08 +0100

    :heavy_plus_sign: dot-notation #64

--- rules/best_practices.js
+++ rules/best_practices.js
@@ -3,4 +3,5 @@ module.exports = {
   'class-methods-use-this': 'off',// https://github.com/dwyl/goodparts/issues/58
   'consistent-return': 'error', // https://github.com/dwyl/goodparts/issues/60
   'default-case': 'error', // https://github.com/dwyl/goodparts/issues/62
+  'dot-notation': 'error', // https://github.com/dwyl/goodparts/issues/64
 };
--- test/fixtures/best_practices.js
+++ test/fixtures/best_practices.js
@@ -4,5 +4,6 @@ module.exports = {
   'array-callback-return': { fail: ['[1].map(function(x){ console.log(x)})'] },
   'class-methods-use-this': null,
   'consistent-return': { fail: ['function f(c) { if (c) { return true; } }'] },
-  'default-case': { fail: ['switch (a) { case 1: break; }'] }
+  'default-case': { fail: ['switch (a) { case 1: break; }'] },
+  'dot-notation': { fail: ['var x; var y = x[\'key\'];'] },
 };
