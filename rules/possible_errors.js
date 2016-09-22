dafa070b3a0260e6b61cb36b73a609cf86e6a3da
Elias <elias0789@gmail.com>
2016-09-22 19:16:51 +0100

    fill in rules for possible errors

--- rules/possible_errors.js
+++ rules/possible_errors.js
@@ -1,1 +1,19 @@
-module.exports = {};
+module.exports = {
+  "valid-jsdoc": ["error", {
+    "prefer": { "arg": "param", "argument": "param", "return": "returns" }
+  }],
+  "no-irregular-whitespace": ["error", {
+    "skipStrings": true,
+    "skipComments": true,
+    "skipRegExps": true,
+    "skipTemplates": true
+  }],
+  "no-func-assign": ["error", "never"],
+  "no-extra-boolean-cast": "error",
+  "no-empty": ["error", "never"],
+  "no-duplicate-case": ["error", "never"],
+  "no-dupe-args": ["error", "never"],
+  "no-control-regex": "error",
+  "no-constant-condition": ["error", "never"],
+  "no-cond-assign": ["error", "never"]
+};
