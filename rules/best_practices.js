777ac79a5e648b54a1855f80f2e8f1253290de12
Jack Rans <jwrans@gmail.com>
2016-09-22 17:40:01 +0100

    :heavy_plus_sign: class-methods-use-this #58

--- rules/best_practices.js
+++ rules/best_practices.js
@@ -1,3 +1,4 @@
 module.exports = {
   'array-callback-return': 'error', // https://github.com/dwyl/goodparts/issues/57
+  'class-methods-use-this': 'off',// https://github.com/dwyl/goodparts/issues/58
 };
--- test/fixtures/best_practices.js
+++ test/fixtures/best_practices.js
@@ -2,4 +2,5 @@ var read = require('../read.js');
 
 module.exports = {
   'array-callback-return': { fail: ['[1].map(function(x){ console.log(x)})'] },
+  'class-methods-use-this': null, // https://github.com/dwyl/goodparts/issues/58
 };
