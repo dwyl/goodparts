9430d98c81e14b3b9bdbce518b5b8f00e9eef919
shouston3 <shouston3@me.com>
2016-09-24 00:16:30 +0100

    adding links to each issue in rules/best_practices.js

--- rules/best_practices.js
+++ rules/best_practices.js
@@ -1,35 +1,35 @@
 module.exports = {
-  "yoda": ["error", "never", { exceptRange: true }],
-  "vars-on-top": "error",
-  "no-with": "error",
-  "no-void": "error",
-  "no-unmodified-loop-condition": "error",
-  "no-useless-concat": "error",
-  "no-unused-labels": "error",
-  "no-sequences": "error",
-  "no-self-assign": ["error", { props: true }],
-  "no-return-assign": ["error", "always"],
-  "no-proto": "error",
-  "no-octal": "error",
-  "no-new": "error",
-  "no-new-func": "error",
-  "no-multi-spaces": "error",
-  "no-loop-func": "error",
-  "no-labels": "error",
-  "no-invalid-this": "error",
-  "no-implicit-globals": "error",
-  "no-global-assign": "error",
-  "no-fallthrough": "error",
-  "no-extra-bind": "error",
-  "no-eval": "error",
-  "no-empty-pattern": "error",
-  "no-else-return": "error",
-  "no-case-declarations": "error",
-  "no-alert": "error",
-  "eqeqeq": ["error", "always"],
-  "dot-location": ["error", "property"],
-  "curly": "error",
-  "complexity": ["error", 10],
-  "block-scoped-var": "error",
-  "accessor-pairs": "error"
+  "accessor-pairs": "error", // https://github.com/dwyl/goodparts/issues/59
+  "block-scoped-var": "error", // https://github.com/dwyl/goodparts/issues/61
+  "complexity": ["error", 10], // https://github.com/dwyl/goodparts/issues/63
+  "curly": "error", // https://github.com/dwyl/goodparts/issues/65
+  "dot-location": ["error", "property"], // https://github.com/dwyl/goodparts/issues/68
+  "eqeqeq": ["error", "always"], // https://github.com/dwyl/goodparts/issues/71
+  "no-alert": "error", // https://github.com/dwyl/goodparts/issues/72
+  "no-case-declarations": "error", // https://github.com/dwyl/goodparts/issues/74
+  "no-else-return": "error", // https://github.com/dwyl/goodparts/issues/87
+  "no-empty-pattern": "error", // https://github.com/dwyl/goodparts/issues/88
+  "no-eval": "error", // https://github.com/dwyl/goodparts/issues/89
+  "no-extra-bind": "error", // https://github.com/dwyl/goodparts/issues/90
+  "no-fallthrough": "error", // https://github.com/dwyl/goodparts/issues/95
+  "no-global-assign": "error", // https://github.com/dwyl/goodparts/issues/97
+  "no-implicit-globals": "error", // https://github.com/dwyl/goodparts/issues/110
+  "no-invalid-this": "error", // https://github.com/dwyl/goodparts/issues/115
+  "no-labels": "error", // https://github.com/dwyl/goodparts/issues/120
+  "no-loop-func": "error", // https://github.com/dwyl/goodparts/issues/125
+  "no-multi-spaces": "error", // https://github.com/dwyl/goodparts/issues/130
+  "no-new-func": "error", // https://github.com/dwyl/goodparts/issues/131
+  "no-new": "error", // https://github.com/dwyl/goodparts/issues/133
+  "no-octal": "error", // https://github.com/dwyl/goodparts/issues/137
+  "no-proto": "error", // https://github.com/dwyl/goodparts/issues/142
+  "no-return-assign": ["error", "always"], // https://github.com/dwyl/goodparts/issues/147
+  "no-self-assign": ["error", { props: true }], // https://github.com/dwyl/goodparts/issues/159
+  "no-sequences": "error", // https://github.com/dwyl/goodparts/issues/163
+  "no-unused-labels": "error", // https://github.com/dwyl/goodparts/issues/166
+  "no-useless-concat": "error", // https://github.com/dwyl/goodparts/issues/172
+  "no-unmodified-loop-condition": "error", // https://github.com/dwyl/goodparts/issues/178
+  "no-void": "error", // https://github.com/dwyl/goodparts/issues/179
+  "no-with": "error", // https://github.com/dwyl/goodparts/issues/181
+  "vars-on-top": "error", // https://github.com/dwyl/goodparts/issues/182
+  "yoda": ["error", "never", { exceptRange: true }] // https://github.com/dwyl/goodparts/issues/183
 };
