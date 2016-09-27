48f8a7efab478a85a5179fec0a7407e882efd789
Elias <elias0789@gmail.com>
2016-09-27 15:18:45 +0100

    add auto-fix support to the bin-file

--- bin/cmd.js
+++ bin/cmd.js
@@ -7,7 +7,18 @@ var eslint = require('eslint');
 var configFilePath = path.resolve(__dirname, '..', '.eslintrc.js');
 var cli, report;
 
-cli = new eslint.CLIEngine({ configFile: configFilePath });
-report = cli.executeOnFiles(process.argv.slice(2));
+var dir = process.argv.slice(2, 3);
+var flags = process.argv.slice(3);
+var shouldFix = flags.indexOf('--fix') > -1;
+
+cli = new eslint.CLIEngine({
+  configFile: configFilePath, fix: shouldFix
+});
+
+report = cli.executeOnFiles(dir);
+
+if (shouldFix) {
+  eslint.CLIEngine.outputFixes(report);
+}
 
 console.log(cli.getFormatter()(report.results)); // eslint-disable-line
