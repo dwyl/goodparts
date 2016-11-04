5e2e3fde19144f3e8f3779df93c9a2dfad0d3c3a
Elias <elias0789@gmail.com>
2016-11-04 16:51:17 +0000

    use test utils to simulate CLI finding linter errors; test that process exits with code 1; issue #266

--- test/bin/cmd.test.js
+++ test/bin/cmd.test.js
@@ -1,19 +1,26 @@
 'use strict';
 
 var test = require('tape');
-var cli = require('../../bin/cmd.js');
+var utils = require('./utils.js');
 var exec = require('child_process').exec;
 
 test('cli executes without error', function (t) {
-  cli(['./rules']);
-  t.end();
+  exec('./bin/cmd.js .', function (err, stdout, stderr) {
+    t.notOk(err, 'Expect no errors thrown');
+    t.notOk(stderr, 'Expect nothing logged to stderr');
+    t.end();
+  });
 });
 
 test('cli exits with code 1 when there are errors', function (t) {
-  exec('./bin/cmd.js .', function (err, stdout, stderr) {
-    t.ok(err, 'Expect process to exit with code 1');
-    t.ok(stdout, 'Expect errors to be logged to stdout');
+  utils.createFile();
+
+  exec('./bin/cmd.js ./test/bin', function (err, stdout, stderr) {
+    t.equal(err.code, 1, 'Expect process to exit with code 1');
+    t.ok(stdout.replace('\n', ''), 'Expect errors to be logged to stdout');
     t.notOk(stderr, 'Don\'t expect anything to be logged to stderr');
+
+    utils.deleteFile();
     t.end();
   });
 });
