49c76f09459ec64b5dbcb4cd9ed6cb9aa099ddca
Elias <elias0789@gmail.com>
2016-11-05 10:28:10 +0000

    make the file-ops utils a bit more generic

--- test/bin/cmd.test.js
+++ test/bin/cmd.test.js
@@ -2,6 +2,7 @@
 
 var test = require('tape');
 var utils = require('./utils.js');
+var path = require('path');
 var exec = require('child_process').exec;
 
 test('cli executes without error', function (t) {
@@ -13,14 +14,17 @@ test('cli executes without error', function (t) {
 });
 
 test('cli exits with code 1 when there are errors', function (t) {
-  utils.createFile();
+  var source = path.resolve(__dirname, '..', 'fixtures', 'ident.fail.js');
+  var target = path.join(__dirname, 'dummy.js');
+
+  utils.copyFile(source, target);
 
   exec('./bin/cmd.js ./test/bin', function (err, stdout, stderr) {
     t.equal(err.code, 1, 'Expect process to exit with code 1');
     t.ok(stdout.replace('\n', ''), 'Expect errors to be logged to stdout');
     t.notOk(stderr, 'Don\'t expect anything to be logged to stderr');
 
-    utils.deleteFile();
+    utils.deleteFile(target);
     t.end();
   });
 });
